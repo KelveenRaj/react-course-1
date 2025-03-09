c71f0da35fc4e05b7aad871015f34f36b9f3c780
Kelveen Raj <kelveenrajv@gmail.com>
2025-03-09 13:02:33 +0800

    week 2

---
+++ week-2/src/components/NavBar/index.js
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavbarContainer = styled.nav`
+  background: orange;
+  padding: 1rem;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const NavItem = styled.li``;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-size: 1.2rem;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: red;
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <NavList>
+        <NavItem>
+          <NavLink to="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/about">About</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/contact">Contact</NavLink>
+        </NavItem>
+      </NavList>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
